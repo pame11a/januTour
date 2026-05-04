979eb225d99437422d9ebf3debe4f6c1a00f9f99
ThallesMaia <thallespim@gmail.com>
2026-05-04 14:28:36 -0300

    style: refina respiro e margens da WelcomeScreen

--- src/components/WelcomeScreen/index.js
+++ src/components/WelcomeScreen/index.js
@@ -11,6 +11,7 @@ export function WelcomeScreen({ onEnter }) {
             style={styles.pattern}
             resizeMode="repeat"
         />
+        
         <View style={styles.logoShadowContainer}>
             <View style={styles.logoRoundWrapper}>
             <Image 
@@ -23,26 +24,27 @@ export function WelcomeScreen({ onEnter }) {
 
       <View style={styles.textContainer}>
         <Text style={styles.title}>JanuTour</Text>
+        
         <Text style={styles.subtitle}>
           O que é o app Janu Tour?
         </Text>
 
-        <Text>
+        <Text style={styles.bodyText}>
           O Janu Tour é um aplicativo desenvolvido para turistas e interessados em explorar o Centro Histórico de Januária. A proposta é oferecer uma experiência guiada que permita ao usuário conhecer, de forma acessível e envolvente, o rico patrimônio arquitetônico da cidade, cuja preservação contribui para contar a história e a identidade cultural do município.
         </Text>   
-        <Text>
+
+        <Text style={styles.bodyText}>
          Em 2024, o Centro Histórico de Januária foi oficialmente tombado pelo Instituto Estadual do Patrimônio Histórico e Artístico de Minas Gerais (IEPHA-MG), reconhecendo seu valor histórico, cultural e arquitetônico.
          </Text>
-
       </View>
 
-        <TouchableOpacity 
-            style={styles.button} 
-            onPress={onEnter}
-            activeOpacity={0.8}
-        >
+      <TouchableOpacity 
+          style={styles.button} 
+          onPress={onEnter}
+          activeOpacity={0.8}
+      >
         <Text style={styles.buttonText}>ENTRAR</Text>
-        </TouchableOpacity>
+      </TouchableOpacity>
     </View>
   );
 }
